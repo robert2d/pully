7ef603e52215603bcc5e4cd15a5b1056de2547d8
Dave Robertson <david.robertson@rea-group.com>
2015-07-29 13:35:46 +1000

    Pull logic

--- lib/pully.rb
+++ lib/pully.rb
@@ -1,7 +1,9 @@
 require 'git'
+require 'uri'
+require 'uri/ssh_git'
 
 class Pully
-  attr_reader :git
+  attr_reader :git, :branch, :remote
 
   def self.open
     new.open
@@ -11,5 +13,30 @@ class Pully
     dir  = '.'
     dir += '/..' until File.directory?("#{dir}/.git")
     @git = Git.open(dir)
+    @branch = Git::Lib.new.send(:command, 'rev-parse --abbrev-ref HEAD')
+    @remote = process_remote(git.config["remote.origin.url"])
+  end
+
+  def open
+    link = "#{remote.gsub('.git', '')}/compare/#{branch}?expand=1"
+    if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
+      system "start #{link}"
+    elsif RbConfig::CONFIG['host_os'] =~ /darwin/
+      system "open #{link}"
+    elsif RbConfig::CONFIG['host_os'] =~ /linux|bsd/
+      system "xdg-open #{link}"
+    end
+  end
+
+  private
+
+  def process_remote(remote)
+    begin
+      URI.parse(remote)
+      return remote
+    rescue URI::InvalidURIError
+      uri = URI::SshGit.parse(remote)
+      return "https://#{uri.host}#{uri.path}"
+    end
   end
 end
